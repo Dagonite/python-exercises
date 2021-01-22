c8150082f0e796081a54514e3c2e73ca1004261b
Dagonite <up821535@myport.ac.uk>
2021-01-22 01:14:16 +0000

    Use lower case for list name
    List isn't constant so use lower case name

--- Exercises/Playing Cards/draw_cards_from_deck.py
+++ Exercises/Playing Cards/draw_cards_from_deck.py
@@ -5,13 +5,13 @@ from random import choice, shuffle
 
 RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace")
 SUITS = ("Diamonds", "Hearts", "Clubs", "Spades")
-CARDS = [rank + " of " + suit for rank in RANKS for suit in SUITS]
-shuffle(CARDS)
+cards = [rank + " of " + suit for rank in RANKS for suit in SUITS]
+shuffle(cards)
 
-first_card = choice(CARDS)
-CARDS.remove(first_card)
+first_card = choice(cards)
+cards.remove(first_card)
 print("Your first card is the", first_card)
 
-second_card = choice(CARDS)
-CARDS.remove(second_card)
+second_card = choice(cards)
+cards.remove(second_card)
 print("\nYour second card is the", second_card)
