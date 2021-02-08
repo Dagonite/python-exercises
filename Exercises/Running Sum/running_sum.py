1311f9fc290bc9db15587ea5b622b7333ad5849a
Dagonite <up821535@myport.ac.uk>
2021-02-08 18:20:12 +0000

    Create running_sum.py

---
+++ Exercises/Running Sum/running_sum.py
@@ -0,0 +1,21 @@
+# running_sum.py
+"""Given a list, nums, return the running sum of nums. We define a running sum of an array as running_sum[i] = 
+sum(nums[0]…nums[i])."""
+
+# could also use accumulate from itertools
+def running_sum(nums):
+    i = 1
+    while i < len(nums):
+        nums[i] += nums[i - 1]
+        i += 1
+    return nums
+
+
+if __name__ == "__main__":
+    test_cases = [[1, 2, 3, 4], [4, 5, 12, 11]]
+
+    answers = [[1, 3, 6, 10], [4, 9, 21, 32]]
+
+    for i, test_case in enumerate(test_cases):
+        res = running_sum(test_case)
+        print("[Pass]" if answers[i] == res else "[Fail]", res)
