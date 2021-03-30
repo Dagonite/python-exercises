6b9293a8868a21a1c1cc1b8499cf1d04fc4ad83d
Dagonite <up821535@myport.ac.uk>
2021-03-31 00:55:04 +0100

    Update odd_numbers.py
    -uncast numbers from list
    -fix join method

--- Exercises/01 Easy/07 Odd Numbers/odd_numbers.py
+++ Exercises/01 Easy/07 Odd Numbers/odd_numbers.py
@@ -1,27 +1,27 @@
 # odd_numbers.py
 """Print all odd numbers between 1 and 100 on separate lines."""
 
-list_of_numbers = list(range(1, 101))
+numbers = range(1, 101)
 
 # using modulus
-for n in list_of_numbers:
+for n in numbers:
     if n % 2:
         print(n)
 
 # using filter and lambda
-for n in filter(lambda n: n % 2, list_of_numbers):
+for n in filter(lambda n: n % 2, numbers):
     print(n)
 
 # using a bitwise operator
-for n in list_of_numbers:
+for n in numbers:
     if n & 1:
         print(n)
 
 # using the join method
-print("".join(str(n) + "\n" for n in list_of_numbers if n % 2))
+print("\n".join(str(n) for n in numbers if n % 2))
 
 # using slice notation
-for n in list_of_numbers[::2]:
+for n in numbers[::2]:
     print(n)
 
 # using a third range argument
