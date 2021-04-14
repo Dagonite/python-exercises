d16a28bc1fc2579401f10b1e8eade20cff545831
Dagonite <up821535@myport.ac.uk>
2021-04-14 20:05:53 +0100

    Make fizzbuzz one word

--- Exercises/01 Easy/16 FizzBuzz/nth_term_fizz_buzz.py
+++ Exercises/01 Easy/16 FizzBuzz/nth_term_fizz_buzz.py
@@ -1,9 +1,9 @@
-# nth_term_fizz_buzz.py
+# nth_term_fizzbuzz.py
 """Create a function that takes an integer, n, and returns "Fizz" if it is a multiple of 3, "Buzz" if it is a multiple of 5,
 "FizzBuzz" if it is a multiple of both, or n as a string if neither."""
 
 
-def fizz_buzz(n):
+def fizzbuzz(n):
     return "Fizz" * (n % 3 == 0) + "Buzz" * (n % 5 == 0) or str(n)
 
 
@@ -31,5 +31,5 @@ if __name__ == "__main__":
     ]
 
     for i, test_case in enumerate(test_cases):
-        res = fizz_buzz(test_case)
+        res = fizzbuzz(test_case)
         print("[Pass]" if answers[i] == res else "[Fail]", res)
