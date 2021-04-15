a7d0a724820f273f22957458d710bdff5066e47d
Dagonite <up821535@myport.ac.uk>
2021-04-15 12:31:43 +0100

    Update modified_fizzbuzz.py

--- Exercises/01 Easy/16 FizzBuzz/modified_fizzbuzz.py
+++ Exercises/01 Easy/16 FizzBuzz/modified_fizzbuzz.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     test_cases = [
         "3 5 10",
         "2 7 15",
-        "5 6 24",
+        "5 6 30",
     ]
 
     answers = [
         "1 2 F 4 B F 7 8 F B",
         "1 F 3 F 5 F B F 9 F 11 F 13 FB 15",
-        "1 2 3 4 F B 7 8 9 F 11 B 13 14 F 16 17 B 19 F 21 22 23 B",
+        "1 2 3 4 F B 7 8 9 F 11 B 13 14 F 16 17 B 19 F 21 22 23 B F 26 27 28 29 FB",
     ]
 
     for i, test_case in enumerate(test_cases):
