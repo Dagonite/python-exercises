e180372112894d4e93089a4a1f499b67218e2e9c
Dagonite <up821535@myport.ac.uk>
2021-04-17 18:10:18 +0100

    Remove excess code in join method argument
    -remove formatter off comment
    -uncast BALLS from a list

--- Exercises/02 Medium/05 Lotterry Balls/lottery_balls.py
+++ Exercises/02 Medium/05 Lotterry Balls/lottery_balls.py
@@ -1,14 +1,13 @@
 # lottery_balls.py
-# fmt: off
 """Script which picks 6 random Lottery balls."""
 
 from random import sample
 
-BALLS = list(range(1, 61))
+BALLS = range(1, 61)
 chosen_balls = sample(BALLS, 6)
 chosen_balls.sort()
 
 print(
-    "Your chosen balls are:", 
-    " - ".join(str(ball) if i < len(chosen_balls) - 1 else str(ball)for i, ball in enumerate(chosen_balls))
-    )
+    "Your chosen balls are:",
+    " - ".join(str(ball) for ball in chosen_balls),
+)
