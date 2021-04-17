2fbb7d0a76db31458ef714f6b545e7ee681a0461
Dagonite <up821535@myport.ac.uk>
2021-04-17 19:29:11 +0100

    Create alternate_zfill.py

---
+++ Exercises/01 Easy/19 Alternate zfill/alternate_zfill.py
@@ -0,0 +1,4 @@
+"""Make a script that generates a list of numbers 0 to 999 as strings. Strings less than 3 characters long should have 
+0s added to the beginning until they reach 3 characters long. The zfill() method can't be used."""
+
+print([f"{n:03}" for n in range(1000)])
