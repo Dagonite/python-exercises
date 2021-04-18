094d03b71f875498f6fc41d42699af560bd8accc
Dagonite <up821535@myport.ac.uk>
2021-04-18 21:39:37 +0100

    Create roman_numerals_encoder.py

---
+++ Exercises/02 Medium/15 Roman Numerals Encoder/roman_numerals_encoder.py
@@ -0,0 +1,65 @@
+# roman_numerals_encoder.py
+"""Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral 
+representation of that integer."""
+
+
+def roman_numerals_encoder(n):
+    SYMBOLS = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "XL": 40,
+        "L": 50,
+        "XC": 90,
+        "C": 100,
+        "CD": 400,
+        "D": 500,
+        "CM": 900,
+        "M": 1000,
+    }
+
+    line = ""
+
+    for symbol in reversed(SYMBOLS):
+        quotient = n // SYMBOLS[symbol]
+        no_of_symbol = min(3, quotient)
+        line += symbol * no_of_symbol
+        n -= no_of_symbol * SYMBOLS[symbol]
+
+    return line
+
+
+if __name__ == "__main__":
+    test_cases = [
+        1,
+        4,
+        6,
+        14,
+        21,
+        89,
+        91,
+        984,
+        1000,
+        1889,
+        1989,
+    ]
+
+    answers = [
+        "I",
+        "IV",
+        "VI",
+        "XIV",
+        "XXI",
+        "LXXXIX",
+        "XCI",
+        "CMLXXXIV",
+        "M",
+        "MDCCCLXXXIX",
+        "MCMLXXXIX",
+    ]
+
+    for i, test_case in enumerate(test_cases):
+        res = roman_numerals_encoder(test_case)
+        print("[Pass]" if answers[i] == res else "[Fail]", test_case, res)
