e9baeb8f86b6930054c515a3d3e4839a19cd1f6b
Dagonite <up821535@myport.ac.uk>
2021-04-18 18:47:44 +0100

    Put testing code into driver func

--- Exercises/02 Medium/09 Sum Between Ranges/sum_between_ranges.py
+++ Exercises/02 Medium/09 Sum Between Ranges/sum_between_ranges.py
@@ -3,50 +3,50 @@
 if x is 1 and y is 5, then sum 1+2+3+4+5."""
 
 
-# imperatively
 def sbr_imp(x, y):
+    """Sum between range imperatively."""
     total = 0
     for n in range(x, y + 1):
         total += n
     return total
 
 
-# using reduce and lambda
 def sbr_reduce(x, y):
+    """Sum between range using reduce and lambda."""
     from functools import reduce
 
     return reduce(lambda n, m: n + m, range(x, y + 1))
 
 
-# using sum()
 def sbr_sum(x, y):
+    "Sum between range using sum()."
     return sum([n for n in range(x, y + 1)])
 
 
 if __name__ == "__main__":
-    test_cases = [
-        (5, 10),
-        (8, 11),
-        (1, 4),
+    sbr_funcs = [
+        sbr_imp,
+        sbr_reduce,
+        sbr_sum,
     ]
 
-    answers = [
-        45,
-        38,
-        10,
-    ]
+    def call_func(func):
+        test_cases = [
+            (5, 10),
+            (8, 11),
+            (1, 4),
+        ]
+        answers = [
+            45,
+            38,
+            10,
+        ]
 
-    print("Imperatively:")
-    for i, test_case in enumerate(test_cases):
-        res = sbr_imp(test_case[0], test_case[1])
-        print([answers[i] == res], res)
+        for i, test_case in enumerate(test_cases):
+            res = func(test_case[0], test_case[1])
+            print([answers[i] == res], res)
 
-    print("\nUsing Reduce and lambda:")
-    for i, test_case in enumerate(test_cases):
-        res = sbr_reduce(test_case[0], test_case[1])
-        print([answers[i] == res], res)
-
-    print("\nUsing sum():")
-    for i, test_case in enumerate(test_cases):
-        res = sbr_sum(test_case[0], test_case[1])
-        print([answers[i] == res], res)
+    for sbr_func in sbr_funcs:
+        print(sbr_func.__doc__)
+        call_func(sbr_func)
+        print()
