d7980d0f1bd0c048be4fcadd7b15f9052a0457a2
Dagonite <up821535@myport.ac.uk>
2021-04-19 19:04:53 +0100

    Correct comments

--- Exercises/01 Easy/14 Vertical Name/vertical_name.py
+++ Exercises/01 Easy/14 Vertical Name/vertical_name.py
@@ -7,14 +7,14 @@ def vertical_name_1(name):
         print(ch)
 
 
-# unpacking name
+# using unpacking and sep arg for print()
 def vertical_name_2(name):
     print(*name, sep="\n")
 
 
-# using join method
+# using join() method
 def vertical_name_3(name):
     print("\n".join(list(name)))
 
 
-vertical_name_3("Sameul Olo")
+vertical_name_3("Samuel Olo")
