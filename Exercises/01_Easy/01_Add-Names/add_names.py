0057e8fa5b005056121d508eb6204551a89dc434
Dagonite <up821535@myport.ac.uk>
2021-05-19 16:07:22 +0100

    Update add_names.py

--- Exercises/01_Easy/01_Add-Names/add_names.py
+++ Exercises/01_Easy/01_Add-Names/add_names.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     test_cases = [
         ({}, "Jamie", 2000),
         ({"Caligula": 37}, "Nero", 54),
-        ({"Robert the Bruce": 1306, "William Wallace": 1270}, "King Edward", 1239),
+        ({"Robert the Bruce": 1274, "William Wallace": 1270}, "King Edward", 1239),
     ]
 
     answers = [
         {"Jamie": 2000},
         {"Caligula": 37, "Nero": 54},
-        {"Robert the Bruce": 1306, "William Wallace": 1270, "King Edward": 1239},
+        {"Robert the Bruce": 1274, "William Wallace": 1270, "King Edward": 1239},
     ]
 
     for i, test_case in enumerate(test_cases):
