6a42cbbcd6cb7acde9ec19bc9fdbffdfaa844e05
Dagonite <up821535@myport.ac.uk>
2021-05-19 02:18:12 +0100

    Correct header string

--- Exercises/01_Easy/17_Print-Array-Elements/print_array_elements.py
+++ Exercises/01_Easy/17_Print-Array-Elements/print_array_elements.py
@@ -1,18 +1,18 @@
 # print_array_elements.py
-"""Give three arrays (a, b, and c) of the same length, containing integers. Loop over the arrays and print their 
-contents. For these numbers the order they are in across the arrays does not matter, so (1, 2, 2) == (2, 2, 1). Do not
-print duplicates."""
+"""Given three arrays of the same length containing integers, loop over the arrays and print the three values at each
+index. If one index produces the values (1, 2, 2) and a subsequent index produces the values (2, 1, 2); do not print the 
+latter combination as it contains the same values as the former."""
 
 a = [1, 1, 1, 2, 8, 6, 1]
 b = [2, 4, 6, 1, 4, 18, 61]
 c = [2, 8, 18, 2, 1, 1, 8]
 
-print("a      b      c")
-combinations = []
+print((" " * 7).join("abc"))
 
-for i in range(len(min(a, b, c))):
+combinations = []
+for i in range(len(a)):
     sorted_combination = sorted((a[i], b[i], c[i]))
 
     if sorted_combination not in combinations:
         combinations.append(sorted_combination)
-        print(f"{a[i]:<7}{b[i]:<7}{c[i]}")
+        print(f"{a[i]:<8}{b[i]:<8}{c[i]}")
