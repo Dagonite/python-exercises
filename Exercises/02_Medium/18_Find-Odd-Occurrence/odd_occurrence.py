b54ee83b9909cfab0e2b2285033554fd6415687e
Dagonite <up821535@myport.ac.uk>
2021-05-21 13:41:33 +0100

    Update odd_occurrence.py

--- Exercises/02_Medium/18_Find-Odd-Occurrence/odd_occurrence.py
+++ Exercises/02_Medium/18_Find-Odd-Occurrence/odd_occurrence.py
@@ -40,10 +40,10 @@ if __name__ == "__main__":
 
     test_cases = []
     for i in range(5):
-        list_1 = [randrange(1, 10) for _ in range(8000)]
+        list_1 = [randrange(1, 10) for _ in range(1, 8000)]
         list_2 = list_1[:]
         n = randrange(1, 10)
-        list_3 = [n] * randrange(1, 6, 2)
+        list_3 = [n] * randrange(1, 2000, 2)
         seq = list_1 + list_2 + list_3
         shuffle(seq)
         test_cases.append(seq)
