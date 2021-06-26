e82a696e4f114b34dfec2ec1dfc0bf7ed154efac
Dagonite <up821535@myport.ac.uk>
2021-06-26 15:51:49 +0100

    Change description

--- Exercises/E02_Medium/E10_Min_Steps/min_steps.py
+++ Exercises/E02_Medium/E10_Min_Steps/min_steps.py
@@ -1,6 +1,30 @@
-"""Alexa is given n piles of equal or unequal heights. In one step, Alexa can remove any number of boxes from the pile 
-which has the maximum height and try to make it equal to the one which is just lower than the maximum height of the 
-stack. Determine the minimum number of steps required to make all of the piles equal in height."""
+"""Alexa is given n piles of boxes which are of equal or unequal heights. In one step, Alexa can remove any number of 
+boxes from the pile which has the maximum height and try to make it equal to the one which is just lower than the 
+maximum height of the stack. Determine the minimum number of steps required to make all of the piles equal in height. 
+For example:
+
+Step 0:
+1 ||||
+2 ||
+3 ||||||
+
+Step 1:
+1 ||||
+2 ||
+3 ||||
+
+Step 1:
+1 ||
+2 ||
+3 ||||
+
+Step 3:
+1 ||
+2 ||
+3 ||
+
+Total steps: 3
+"""
 
 
 def min_steps(piles):
@@ -13,9 +37,9 @@ def min_steps(piles):
 
 
 if __name__ == "__main__":
-    test_cases = [[1, 2, 3], [4, 5, 12, 11], [8, 2, 1, 5, 6, 6]]
+    test_cases = [[1, 2, 3], [4, 2, 6], [4, 5, 12, 11], [8, 2, 1, 5, 6, 6]]
 
-    answers = [3, 6, 13]
+    answers = [3, 3, 6, 13]
 
     for i, test_case in enumerate(test_cases):
         res = min_steps(test_case)
