0ff5bb08e1227f42c25bfe740749f0136d61b5dd
Dagonite <up821535@myport.ac.uk>
2021-08-09 20:54:48 +0100

    Add class decorators example

---
+++ Classes/C05_Class_Decorators/location.py
@@ -0,0 +1,20 @@
+from position import EarthPosition
+from utility import auto_repr
+
+
+@auto_repr
+class Location:
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def position(self):
+        return self._position
+
+    def __str__(self):
+        return self.name
